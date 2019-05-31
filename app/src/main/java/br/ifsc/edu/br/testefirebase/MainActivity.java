1a84d50220562f6bc69d8bcf69ec86e58735a596
aluno <adsroot.aluno.ifsc.edu.br>
2019-05-31 18:49:27 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/br/testefirebase/MainActivity.java
@@ -0,0 +1,31 @@
+package br.ifsc.edu.br.testefirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
+    private EditText edNome, edApelido, dtNascimento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        reference.child("teste").setValue("100");
+        reference.child("usuarios").child("id").child("01").child("email").setValue("larissa@gmail.com");
+        reference.child("usuarios").child("id").child("02").child("email").setValue("bruno@gamil.com");
+        reference.child("usuarios").child("id").child("03").child("email").setValue("bianca@gmail.com");
+
+    }
+}
